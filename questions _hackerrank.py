0682be931b52e4bdf592b2817a2a96cc0a20b906
pragam-m25 <pragam279@gmail.com>
2025-11-29 18:39:49 +0530

    python guess game

--- questions _hackerrank.py
+++ questions _hackerrank.py
@@ -7,3 +7,21 @@
 #          arr.remove(a) 
 #          b=max(arr) 
 #          print(b)
+
+import random 
+num=random.randint(1,100)
+print(" hi ,please chosse number b/w 1 to 100")
+guesses=1
+user_input=-1
+while(user_input != num):
+ user_input=int(input(" enter your guess: "))
+
+ if user_input < num :
+        print(f"number is greater than your guess")
+        guesses+=1
+ elif user_input > num:
+      print("number is less than your guess")
+      guesses+=1
+
+print(f"congratulation your guess is correct number {num},you take {guesses} attempt ")
+
